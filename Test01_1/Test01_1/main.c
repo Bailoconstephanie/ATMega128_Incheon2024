d39f5d6cc76c8f15b0240400e3cd2130b163602b
bailoconstephanie <ghost00014@naver.com>
2024-10-02 15:40:49 +0900

    1

---
+++ Test01_1/Test01_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Test01_1.c
+ *
+ * Created: 2024-10-02 오후 12:41:41
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
